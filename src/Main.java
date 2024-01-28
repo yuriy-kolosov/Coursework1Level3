796cb4b0007e3015ccc4b0dfa143e44c761cca1e
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-01-28 23:25:50 +0300

    Курсовая работа 1. Техническое задание. Уровень 3 - начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Курсовая работа 1. Техническое задание");
+        System.out.println();
+        System.out.println("        Уровень 3. Очень сложно");
+        System.out.println();
+    }
+}
